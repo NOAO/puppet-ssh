0e6c528716392a650e1dae5501b45205d1cb67b7
Joshua Hoblitt <jhoblitt@cpan.org>
2013-06-14 12:41:29 -0700

    replace sshd_config files with sshd_config type resources from augeasproviders

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,12 +6,36 @@ class ssh::config {
     group   => 'root',
   }
 
-  file { $ssh::params::ssh_service_config:
-    ensure  => present,
-    mode    => '0600',
-    source  => "puppet:///modules/ssh/${ssh::params::ssh_service_config_source}",
-    require => Class['ssh::install'],
-    notify  => Class['ssh::service'],
+  sshd_config { 'PermitRootLogin':
+    ensure => present,
+    value  => 'without-password',
+  }
+
+  sshd_config { 'X11Forwarding':
+    ensure => present,
+    value  => 'yes',
+  }
+
+  sshd_config { 'X11UseLocalhost':
+    ensure => present,
+    value  => 'no',
+  }
+
+  sshd_config { 'MaxStartups':
+    ensure => present,
+    value  => '1024',
+  }
+
+  # GPFS requires no motd/etc. is printed by ssh
+  # XXX confine this to just the root user?
+  sshd_config { 'PrintMotd':
+    ensure => present,
+    value  => 'no',
+  }
+
+  sshd_config { 'PrintLastLog':
+    ensure => present,
+    value  => 'no',
   }
 
   file { $ssh::params::ssh_client_config:
